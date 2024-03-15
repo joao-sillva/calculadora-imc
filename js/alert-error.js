1a12e039a068074f80d2a12edf349849ed101abc
joao-sillva <32817506-joao-sillva@users.noreply.replit.com>
2024-03-15 03:42:05 +0000

    Create README.md

---
+++ js/alert-error.js
@@ -0,0 +1,7 @@
+export const AlertError = {
+  element: document.querySelector('.alert-error'),
+  
+  open() {AlertError.element.classList.add('open')},
+  close() {AlertError.element.classList.remove('open')}
+}
+
